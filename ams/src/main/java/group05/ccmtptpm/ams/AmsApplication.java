2b371dafa459f9f18743699ebfdb7364528a575a
23110182-art <23110182@student.hcmute.edu.vn>
2026-04-15 11:10:16 +0700

    ops: bo sung Dockerfile de chay app trong docker

--- ams/src/main/java/group05/ccmtptpm/ams/AmsApplication.java
+++ ams/src/main/java/group05/ccmtptpm/ams/AmsApplication.java
@@ -2,9 +2,16 @@ package group05.ccmtptpm.ams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AmsApplication {
+public class AmsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AmsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmsApplication.class, args);		
